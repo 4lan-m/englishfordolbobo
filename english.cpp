32a3bb3662aa483f0fdf9fa9f4983de76eb0bd39
4lan.m <req@4lanm.ml>
2022-05-05 23:29:38 +0300

    Добавил дохуя всего

--- english.cpp
+++ english.cpp
@@ -50,7 +50,9 @@ int main() {
     "неподвижный",
     "длина"
   }; 
-  for (;;) {
+  int masN5[8] = {0,0,0,0,0,0,0,0};
+  std :: cout << "Напиши 0 когда захочешь закончить тренировку..." << std :: endl;
+  for (;test != 0;) {
     n = rand() % 8;
     std::cout << "Форма " << n1[n]  <<"(" << n4[n] <<")"<< ": ";
     std::cin >> test;
@@ -62,5 +64,10 @@ int main() {
         "Ответ был: " << n2[n] << std::endl;
       std::cout << "Перевод слова " << n2[n] << " - " << n3[n] << std::endl;
     }
+    masN5[n]++;
   }
+  std :: cout << "Вы повторили:" << std :: endl;
+  for (int i = 0; i < 8; i++){
+  std :: cout << n1[i] << " - " << n2[i] << masN5 [i] << "раз" << std :: endl;
+  } 
 }
